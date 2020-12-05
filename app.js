f200d82ceb413960fa079cf8bcb245beb8031145
Lorena Arce <you@example.com>
2020-12-05 01:41:09 -0800

    got the values of the default subject ID, working on plotting it

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 //Use d3.json() to fetch the data from the JSON file
 //The data within the json file is arbitrarily called importedData
 d3.json("data/samples.json").then((importedData) => {
-    // console.log(importedData)
+    console.log(importedData)
 
     let data = importedData;
-
-    // data.sort(function(a,b){
-    //     return (d.)
-    // })
 
     let subjectID = data.names;
 
@@ -18,27 +14,48 @@ d3.json("data/samples.json").then((importedData) => {
 
         let section = d3.select("#selDataset");
 
-        section.append("option").text(member)
-    })
-})
-
-// let sample_values = data.samples.sample_values;
-
-// console.log(sample_values);
-
-function unpack(rows, index) {
-    return rows.map(function (row) {
-        return row[index];
+        section.append("option").text(member);
     });
-}
+    // });
 
-// function getData() {
-//     d3.json("data/samples.json").then(function(data) {
-//         let values = unpack(data.samples.sample_values)
+    // Initialize the page with a default plot
+    //We will use subject ID 940 data for default plot
+    function init() {
 
-    // console.log(values)
+        let fileteredID = data.samples.filter(subject => subject.id === "940")[0];
+        // console.log(fileteredID);
 
-    // })
-// console.log(values)
-// }
-// console.log(values)
+        let sampleValues = fileteredID.sample_values;
+        console.log(sampleValues);
+
+        let otuIDs = fileteredID.otu_ids;
+        // console.log(otuIDs);
+
+        let otuLabels = fileteredID.otu_labels;
+        // console.log(otuLabels);
+
+        trace = {
+           x: ,
+           y: ,
+           text: ,
+           name: "Bar Graph",
+           type: "bar",
+           orientation: "h" 
+        }
+
+        let traceData = [trace]
+
+        let layout = {
+            title: "Top 10 OTUs",
+            xaxis: {title: "OTU IDs"},
+            yaxis: {title: "Sample Values"}
+        }
+
+        Plotly.newPlot("bar", traceData, layout);
+    }
+
+    init();
+
+});
+
+
